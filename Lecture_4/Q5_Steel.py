d956ef3a8fb87ce5fa506a2891412013ce0c5b2f
B.Vaibhav <vaibhavbalaji2007@gmail.com>
2026-02-05 09:26:37 +0530

    Renaming files and fixing an error in 5th question in lecture 4

--- Lecture_4/Q5_Steel.py
+++ Lecture_4/Q5_Steel.py
@@ -1,15 +1,15 @@
 hard = int(input("Enter the hard of the steel : "))
-carb = float(input("Enter the carcarbonon content of the steel : "))
+carbon = float(input("Enter the carbon content of the steel : "))
 tens = int(input("Enter the tensile strength of the steel : "))
-if hard > 50 and carb < 0.7 and tens > 5600:
+if hard > 50 and carbon < 0.7 and tens > 5600:
     print("Grade 10 Steel")
-elif hard > 50 and carb < 0.7:
+elif hard > 50 and carbon < 0.7:
     print("Grade 9 Steel")
-elif carb < 0.7 and tens > 5600:
+elif carbon < 0.7 and tens > 5600:
     print("Grade 8 Steel")
 elif hard > 50 and tens > 5600:
     print("Grade 7 Steel")
-elif hard > 50 or carb < 0.7 or tens > 5600:
+elif hard > 50 or carbon < 0.7 or tens > 5600:
     print("Grade 6 Steel")
 else:
     print("Grade 5 Steel")
