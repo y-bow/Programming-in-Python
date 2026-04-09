598230dfb22715cb3b7efcceed1dafbfbb815c8b
B.Vaibhav <125469980+y-bow@users.noreply.github.com>
2026-04-09 10:04:44 +0530

    Update Lecture 19 (Coin Sum)

--- Lecture_19/Recursion_coin_change.py
+++ Lecture_19/Recursion_coin_change.py
@@ -3,16 +3,14 @@ def coin_change(amount, coin):
         return 0
     if amount == 0:
         return 1
-    if coin < 0:
+    if coin == 0:
         return 0
     if coin == 5:
         return coin_change(amount - 5, 5) + coin_change(amount, 2)
     elif coin == 2:
         return coin_change(amount - 2, 2) + coin_change(amount, 1)
-    elif coin == 1:
+    else:
         return coin_change(amount - 1, 1)
-    return 0
 
-amount = 4
-print(coin_change(amount, 5))
-#NOT COMPLETED YET
+amount = int(input("Enter the amount : "))
+print(f"Number of ways to calculate {amount} is {coin_change(amount, 5)}")
