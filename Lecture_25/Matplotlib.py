4954f16ac116d6574790d6c980126f2ab31b3490
B.Vaibhav <125469980+y-bow@users.noreply.github.com>
2026-04-30 09:22:09 +0530

    Updated Lecture 25

--- Lecture_25/Matplotlib.py
+++ Lecture_25/Matplotlib.py
@@ -2,8 +2,10 @@
 import numpy as np
 a = np.random.rand(10)
 print(a)
+#Gives you the 5 random real numbers from 0 to 1 [0,1)
 
-b = np.random.uniform(-2,2)
+b = np.random.uniform(-2,2,5)
 print(b)
+#Going to give 5 random real number between (-1, 1)
 
 c = np.random.randint(1,9)
