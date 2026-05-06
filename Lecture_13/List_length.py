1b754f466e40090a82e6b3b10d9603c0df67229f
B.Vaibhav <125469980+y-bow@users.noreply.github.com>
2026-05-06 23:38:31 +0530

    Update List_length.py

--- Lecture_13/List_length.py
+++ Lecture_13/List_length.py
@@ -3,12 +3,12 @@ l1 = [1,2,3]
 def length(n):
     if isinstance(n, str):
         count = 0
-        for i in l1:
+        for i in n:
             count+=1
         return f"Length of the string is {count}"
     elif isinstance(n,list):
         count = 0
-        for i in l1:
+        for i in n:
             count+=1
         return f"Length of the list is {count}"
     else:
