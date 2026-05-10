32a7ab89590d3531252ff5d2a730037dfaf07851
B.Vaibhav <125469980+y-bow@users.noreply.github.com>
2026-05-10 22:56:16 +0530

    Update Rectangle_Square_Inheritance.py

--- Lecture_21/Rectangle_Square_Inheritance.py
+++ Lecture_21/Rectangle_Square_Inheritance.py
@@ -1,7 +1,7 @@
 class rectangle:
     def __init__(self,length,breadth):
         self.length = length
-        self.breadth=breadth
+        self.breadth = breadth
     def area(self):
         return self.length * self.breadth
 
